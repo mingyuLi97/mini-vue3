7320808033ea016f2ad30340b6dd497f471725f2
limy <mingyuli1997@gmail.com>
2022-09-12 20:08:23 +0800

    feat: scheduler

--- packages/reactivity/src/effect.ts
+++ packages/reactivity/src/effect.ts
@@ -1,4 +1,9 @@
 type EffectFn = () => any;
+// 调度器
+type EffectScheduler = (effect: ReactiveEffect) => any;
+interface EffectOptions {
+  scheduler?: EffectScheduler;
+}
 
 export let activeEffect: ReactiveEffect = null;
 
@@ -6,9 +11,14 @@ export let activeEffect: ReactiveEffect = null;
  * @description:
  * @param {EffectFn} fn 根据状态变化重新执行
  */
-export function effect(fn: EffectFn) {
-  const _effect = new ReactiveEffect(fn);
+export function effect(fn: EffectFn, options: EffectOptions = {}) {
+  const _effect = new ReactiveEffect(fn, options);
   _effect.run(); // 默认执行一次
+
+  // effect函数的返回值就是一个runner 可以让失活的effect再次执行 只是需要手动触发执行了(不会自动开始收集依赖)
+  const runner = _effect.run.bind(_effect);
+  runner.effect = _effect;
+  return runner;
 }
 
 class ReactiveEffect {
@@ -16,7 +26,7 @@ class ReactiveEffect {
   // 为了解决 effect 中嵌套 effect 的问题
   parent: ReactiveEffect = null;
   deps: Set<ReactiveEffect>[] = [];
-  constructor(public fn: EffectFn) {}
+  constructor(public fn: EffectFn, public scheduler?: EffectScheduler) {}
 
   /**
    * 执行 effect 方法
@@ -25,20 +35,29 @@ class ReactiveEffect {
     // 激活状态 才需要收集这个副作用函数fn内用到的响应式数据 也就是我们说的依赖收集
     // 非激活状态 只执行函数 不收集依赖
     if (!this.active) {
-      this.fn();
+      return this.fn();
     }
+    let res;
 
     try {
       this.parent = activeEffect;
       // 激活状态 依赖收集了 核心就是将当前的effect和稍后渲染的属性关联在一起
       activeEffect = this;
       // 执行传入的fn的时候，如果出现了响应式数据的获取操作，就可以获取到这个全局的activeEffect
-      cleanupEffect(this)
-      return this.fn();
+      cleanupEffect(this);
+      res = this.fn();
     } finally {
       // 执行完当前的effect 归还上次 activeEffect 变量指向的值
       activeEffect = this.parent;
       this.parent = null;
+    }
+    return res;
+  }
+
+  stop() {
+    if (this.active) {
+      this.active = false;
+      cleanupEffect(this);
     }
   }
 }
@@ -94,18 +113,22 @@ export const trigger = (
   if (effects) {
     /**
      * [...effects] 防止死循环 刚删除的引用马上又添加进来
-     * 
+     *
      * 这样会死循环
      * set.forEach(()=> {
      *  set.delete(1)
      *  set.add(1)
      * })
-     * 
+     *
      */
     [...effects].forEach((effect) => {
       // 在执行 run 的时候 如果访问到属性，就会继续执行 effect（死循环）
       if (effect === activeEffect) return;
-      effect.run();
+      if (effect.scheduler) {
+        effect.scheduler(effect);
+      } else {
+        effect.run();
+      }
     });
   }
 };
